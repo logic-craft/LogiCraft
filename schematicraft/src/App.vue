bcc87590acab63f57c05d19d686efb6f1b7f12e3
Rafael Goesmann <rgo51@uclive.ac.nz>
2019-07-13 11:50:20 +1200

    Added schematicraft project

---
+++ schematicraft/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Grid />
+  </div>
+</template>
+
+<script>
+import Grid from './components/Grid.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Grid
+  },
+  data() {
+    return {
+      logicGates: "hello world"
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ schematicraft/src/components/Grid.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-stage :config="configKonva">
+      <v-layer>
+        <v-line :config="{
+          points: [Math.round(i * padding) + 0.5, 0, Math.round(i * padding) + 0.5, configKonva.height],
+          stroke: '#ddd',
+          strokeWidth: 1
+        }"
+        v-for="i in configKonva.height / padding"
+        v-bind:key="i" 
+        />
+
+        <v-line :config="{
+          points: [0, 0, 10, 10],
+        }"
+        />
+
+        
+
+        <v-line :config="{
+          points: [0, Math.round(j * padding), configKonva.width, Math.round(j * padding)],
+          stroke: '#ddd',
+          strokeWidth: 1
+        }"
+        v-for="j in configKonva.height / padding"
+        v-bind:key="j" 
+        />
+
+      </v-layer>
+    </v-stage>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      configKonva: {
+        width: 500,
+        height: 500,
+      },
+      configCircle: {
+        x: 100,
+        y: 100,
+        radius: 70,
+        fill: "red",
+        stroke: "black",
+        strokeWidth: 4
+      },
+      padding: 20
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
