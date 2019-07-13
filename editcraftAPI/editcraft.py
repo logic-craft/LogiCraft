d27edee4f100c74641af5e97b217dca5087000d3
campbell mercer <cme69@uclive.ac.nz>
2019-07-13 16:47:33 +1200

    Yoink

--- editcraftAPI/editcraft.py
+++ editcraftAPI/editcraft.py
@@ -5,7 +5,12 @@ def main(world_folder):
     """Create and run editor"""
     editor = Editor(world_folder)
     print(editor.get_block_name(1, 120, 3))
-    editor.set_block(1, 50, 3, "redstone_torch")
+    editor.set_block(0, 51, 4, "redstone_torch")
+    editor.set_block(2, 51, 4, "redstone_torch")
+    editor.set_block(1, 50, 5, "redstone_wall_torch", {"lit": "true", "facing": "south"})
+    editor.set_block(0, 50, 4, "white_wool")
+    editor.set_block(1, 50, 4, "white_wool")
+    editor.set_block(2, 50, 4, "white_wool")
 
 
 if __name__ == "__main__":
