5a2d11fd076349262abcbf19d05041f15d0581d6
Marvin <mgo81@uclive.ac.nz>
2019-07-13 19:20:40 +1200

    implemented placing of redstone

--- logicraftcore/src/endpoint/endpoint.py
+++ logicraftcore/src/endpoint/endpoint.py
@@ -1,18 +1,32 @@
 from flask import request, send_from_directory
 from flask_restful import Resource
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join('../../')))
+
+from models.gate import Gate
+
+
 
 class Schematic(Resource):
     def post(self):
         req = request.get_json()
 
         os.system("ls")
-        os.system("cp -Ri ../maps/LogiCraft ../maps/LogiCraftCopy")
+        os.system("cp -R ../maps/LogiCraft ../maps/LogiCraftCopy")
+
+
+        gate = Gate(1, {
+            "points": [[4, 3], [0, 3]],
+            "output": [4, 5]
+        }, [1, 0])
+
+        gate.place_redstone()
         os.system("cd ../maps/LogiCraftCopy; zip -r ../LogiCraftCopy.zip *")
-        os.system("rm -r ../maps/LogiCraftCopy")
-        map_file = send_from_directory("../maps/", "LogiCraftCopy.zip", as_attachment=True)
-        os.system("rm ../maps/LogiCraftCopy.zip")
-        return map_file
+        # os.system("rm -r ../maps/LogiCraftCopy")
+        # map_file = send_from_directory("../maps/", "LogiCraftCopy.zip", as_attachment=True)
+        # os.system("rm ../maps/LogiCraftCopy.zip")
+        # return map_file
 
     def get(self):
         return send_from_directory("../maps/", "LogiCraftCopy.zip", as_attachment=True) 
--- logicraftcore/src/models/gate.py
+++ logicraftcore/src/models/gate.py
@@ -1,27 +1,53 @@
 from editcraftAPI.editor import Editor
+import os
 
-BASE_Y = 51
+BASE_Y = 50
 
 class Gate:
     def __init__(self, id, inputs, coord):
         self.id = id
+        self.inputs = inputs
         self.coord = coord
-        self.editor = Editor("./logicraftcore/maps/LogiCraftCopy")
+        self.editor = Editor(os.path.abspath(os.path.join("../maps/LogiCraftCopy")))
     
-    def get_input_top_coord(self):
+    def get_input_1_coord(self):
         return [self.coord[0] - 1, self.coord[1] + 2]
 
-    def get_input_middle_coord(self):
-        return [self.coord[0] - 1, self.coord[1] + 1]
-
-    def get_input_bottom_coord(self):
+    def get_input_2_coord(self):
         return [self.coord[0] - 1, self.coord[1]]
+
+    def get_input_coord(self, coord_num=1):
+        if coord_num == 1:
+            return self.get_input_1_coord()
+        elif coord_num == 2:
+            return self.get_input_2_coord()
+
 
     def get_output_coord(self):
         return [self.coord[0] + 4, self.coord[1] + 1]
     
+    def set_redstone(self, coord):
+        self.editor.set_block(coord[1], BASE_Y, coord[0], "red_wool")
+        self.editor.set_block(coord[1], BASE_Y + 1, coord[0], "redstone_wire")
+
     def place_redstone(self):
-        self.editor.set_redstone()
+        for _input in range(1, len(self.inputs) + 1):
+            points = [self.inputs["output"]] + self.inputs["points"] + [self.get_input_coord(_input)]
+            for i in range(len(points) - 1):
+
+                if points[i][0] == points[i + 1][0]:
+                    difference = points[i][1] - points[i + 1][1]
+                    multiplier = difference // abs(difference) # 1 or -1
+                    for j in range(0, difference + multiplier, multiplier):
+                        self.set_redstone([points[i + 1][0], points[i + 1][1] + j])
+
+                elif points[i][1] == points[i + 1][1]:
+                    difference = points[i][0] - points[i + 1][0]
+                    multiplier = difference // abs(difference) # 1 or -1
+                    for j in range(0, difference + multiplier, multiplier):
+                        self.set_redstone([points[i + 1][0] + j, points[i + 1][1]])
+
+
 
 
 
