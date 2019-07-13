44d2708a1f4d1e00bd1989ae2a32c39fe58e329e
campbell mercer <cme69@uclive.ac.nz>
2019-07-14 05:56:03 +1200

    Yoink3.0

--- logicraftcore/src/models/xor_gate.py
+++ logicraftcore/src/models/xor_gate.py
@@ -3,8 +3,8 @@ from models.gate import Gate
 class Xor(Gate):
     
     def place_gate(self):
-        x = self.coord[0]
-        z = self.coord[1]
+        z = self.coord[0]
+        x = self.coord[1]
         self.editor.set_block(x+0, 50, z+0, "orange_wool")
         self.editor.set_block(x+1, 50, z+0, "orange_wool")
         self.editor.set_block(x+2, 50, z+0, "orange_wool")
