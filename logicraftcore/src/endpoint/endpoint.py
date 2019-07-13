4e6bd9ebbdaaf9be93fdccd930773f1c675cfef5
Marvin <mgo81@uclive.ac.nz>
2019-07-13 20:23:38 +1200

    request parsing and 'or' gate generated

--- logicraftcore/src/endpoint/endpoint.py
+++ logicraftcore/src/endpoint/endpoint.py
@@ -4,29 +4,40 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join('../../')))
 
-from models.gate import Gate
-
+from models.and_gate import And
+from models.or_gate import Or
+from models.not_gate import Not 
 
 
 class Schematic(Resource):
     def post(self):
-        req = request.get_json()
-
         os.system("ls")
         os.system("cp -R ../maps/LogiCraft ../maps/LogiCraftCopy")
 
+        req = request.get_json()
 
-        gate = Gate(1, {
-            "points": [[4, 3], [0, 3]],
-            "output": [4, 5]
-        }, [1, 0])
+        for i in range(req):
+            for j in range(req[i].inputs):
+                coord = req[req[i].inputs[j]["id"]]["coordinate"]
+                req[i].inputs[j]["output"] = [coord[0] + 4, coord[1] + 1]
 
-        gate.place_redstone()
-        os.system("cd ../maps/LogiCraftCopy; zip -r ../LogiCraftCopy.zip *")
+            if req[i].type == "AND":
+                gate = And(i, req[i]["inputs"], req[i]["coordinate"])
+                gate.place_redstone() 
+                gate.place_gate()
+            elif req[i].type == "OR":
+                gate = Or(i, req[i]["inputs"], req[i]["coordinate"])
+                gate.place_redstone() 
+                gate.place_gate()
+            elif req[i].type == "NOT":
+                gate = Not(i, req[i]["inputs"], req[i]["coordinate"])
+                gate.place_redstone() 
+                gate.place_gate()
+            else:
+                return "bad request", 400
+
+        # os.system("cd ../maps/LogiCraftCopy; zip -r ../LogiCraftCopy.zip *")
         # os.system("rm -r ../maps/LogiCraftCopy")
         # map_file = send_from_directory("../maps/", "LogiCraftCopy.zip", as_attachment=True)
         # os.system("rm ../maps/LogiCraftCopy.zip")
         # return map_file
-
-    def get(self):
-        return send_from_directory("../maps/", "LogiCraftCopy.zip", as_attachment=True) 
--- logicraftcore/src/models/gate.py
+++ logicraftcore/src/models/gate.py
@@ -1,7 +1,6 @@
 from editcraftAPI.editor import Editor
 import os
 
-BASE_Y = 50
 
 class Gate:
     def __init__(self, id, inputs, coord):
@@ -9,6 +8,7 @@ class Gate:
         self.inputs = inputs
         self.coord = coord
         self.editor = Editor(os.path.abspath(os.path.join("../maps/LogiCraftCopy")))
+        self.BASE_Y = 50
     
     def get_input_1_coord(self):
         return [self.coord[0] - 1, self.coord[1] + 2]
@@ -27,12 +27,13 @@ class Gate:
         return [self.coord[0] + 4, self.coord[1] + 1]
     
     def set_redstone(self, coord):
-        self.editor.set_block(coord[1], BASE_Y, coord[0], "red_wool")
-        self.editor.set_block(coord[1], BASE_Y + 1, coord[0], "redstone_wire")
+        self.editor.set_block(coord[1], self.BASE_Y, coord[0], "red_wool")
+        self.editor.set_block(coord[1], self.BASE_Y + 1, coord[0], "redstone_wire")
 
     def place_redstone(self):
-        for _input in range(1, len(self.inputs) + 1):
-            points = [self.inputs["output"]] + self.inputs["points"] + [self.get_input_coord(_input)]
+        for _input in range(len(self.inputs)):
+            points = [self.inputs[_input]["output"]] + self.inputs[_input]["points"] + [self.get_input_coord(_input + 1)]
+            
             for i in range(len(points) - 1):
 
                 if points[i][0] == points[i + 1][0]:
@@ -46,6 +47,7 @@ class Gate:
                     multiplier = difference // abs(difference) # 1 or -1
                     for j in range(0, difference + multiplier, multiplier):
                         self.set_redstone([points[i + 1][0] + j, points[i + 1][1]])
+
 
 
 
